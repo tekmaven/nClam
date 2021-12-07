4d453cdd5c52e8b5ca29a33811232cb66c3c8513
Kai Leonhardt <8343141+Mik4sa@users.noreply.github.com>
2021-12-07 13:39:24 +0100

    Don't rely on the actual count of read bytes
    Fixes #53.

--- nClam/ClamClient.cs
+++ nClam/ClamClient.cs
@@ -122,19 +122,19 @@
         /// <param name="cancellationToken"></param>
         private async Task SendStreamFileChunksAsync(Stream sourceStream, Stream clamStream, CancellationToken cancellationToken)
         {
-            var size = MaxChunkSize;
-            var bytes = new byte[size];
+            int readByteCount;
+            var bytes = new byte[MaxChunkSize];
 
-            while ((size = await sourceStream.ReadAsync(bytes, 0, size, cancellationToken).ConfigureAwait(false)) > 0)
+            while ((readByteCount = await sourceStream.ReadAsync(bytes, 0, MaxChunkSize, cancellationToken).ConfigureAwait(false)) > 0)
             {
                 if (sourceStream.CanSeek && sourceStream.Position > MaxStreamSize)
                 {
                     throw new MaxStreamSizeExceededException(MaxStreamSize);
                 }
 
-                var sizeBytes = BitConverter.GetBytes(System.Net.IPAddress.HostToNetworkOrder(size));  //convert size to NetworkOrder!
-                await clamStream.WriteAsync(sizeBytes, 0, sizeBytes.Length, cancellationToken).ConfigureAwait(false);
-                await clamStream.WriteAsync(bytes, 0, size, cancellationToken).ConfigureAwait(false);
+                var readBytes = BitConverter.GetBytes(System.Net.IPAddress.HostToNetworkOrder(readByteCount));  //convert readByteCount to NetworkOrder!
+                await clamStream.WriteAsync(readBytes, 0, readBytes.Length, cancellationToken).ConfigureAwait(false);
+                await clamStream.WriteAsync(bytes, 0, readByteCount, cancellationToken).ConfigureAwait(false);
             }
 
             var newMessage = BitConverter.GetBytes(0);
