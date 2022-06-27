def096df8312cc01beeae9020c1bd934157d8af9
barneynicholls <barnaby.nicholls@payroc.com>
2022-06-27 15:06:15 +0100

    Address build warnings and changed ConsoleTest to use local file to upload to ClamAV server

--- nClam.ConsoleTest/Program.cs
+++ nClam.ConsoleTest/Program.cs
@@ -1,45 +1,48 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using nClam;
 
-class Program
+namespace nClam.ConsoleTest
 {
-  static async Task Main(string[] args)
-  {
-    Console.WriteLine("nClam Test Application");
-    Console.WriteLine();
-
-    Console.Write("\t* Testing connectivity: ");
-
-    var clam = new ClamClient("localhost", 3310);
-    var pingResult = await clam.TryPingAsync();
-
-    if (!pingResult)
+    static class Program
     {
-      Console.WriteLine("test failed. Exiting.");
-      return;
+        static async Task Main()
+        {
+            Console.WriteLine("nClam Test Application");
+            Console.WriteLine();
+
+            Console.Write("\t* Testing connectivity: ");
+
+            var clam = new ClamClient("localhost", 3310);
+            var pingResult = await clam.TryPingAsync();
+
+            if (!pingResult)
+            {
+                Console.WriteLine("test failed. Exiting.");
+                return;
+            }
+
+            Console.WriteLine("connected.");
+
+            Console.Write("\t* Scanning file: ");
+            var fileToScan = ConfigurationManager.AppSettings["fileToScan"]; //any file you would like!
+            var scanResult = await clam.SendAndScanFileAsync(fileToScan);
+
+            switch (scanResult.Result)
+            {
+                case ClamScanResults.Clean:
+                    Console.WriteLine("The file is clean!");
+                    break;
+                case ClamScanResults.VirusDetected:
+                    Console.WriteLine("Virus Found!");
+                    Console.WriteLine("Virus name: {0}", scanResult.InfectedFiles.First().VirusName);
+                    break;
+                case ClamScanResults.Error:
+                    Console.WriteLine("Woah an error occured! Error: {0}", scanResult.RawResult);
+                    break;
+            }
+
+        }
     }
-
-    Console.WriteLine("connected.");
-
-    Console.Write("\t* Scanning file: ");
-    var scanResult = await clam.ScanFileOnServerAsync("C:\\test.txt");  //any file you would like!
-
-    switch (scanResult.Result)
-    {
-      case ClamScanResults.Clean:
-        Console.WriteLine("The file is clean!");
-        break;
-      case ClamScanResults.VirusDetected:
-        Console.WriteLine("Virus Found!");
-        Console.WriteLine("Virus name: {0}", scanResult.InfectedFiles.First().VirusName);
-        break;
-      case ClamScanResults.Error:
-        Console.WriteLine("Woah an error occured! Error: {0}", scanResult.RawResult);
-        break;
-    }
-
-  }
 }
